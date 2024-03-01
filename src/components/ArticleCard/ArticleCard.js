8965ae8c720f09afa3ef3ecd887cb439988c87d5
irfan2602 <meirfan26@gmail.com>
2024-03-02 00:54:36 +0530

    changes

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 const ArticleCard = ({ link, title, pulishDate, updateDate, section }) => {
     return (
         <div className='articleCard'>
-            <h4 className='articleTitle'>Source : {title}   </h4>
+            <h4 className='articleTitle'>Source : {title} </h4>
             <div><strong>Published Date : </strong> <span>{pulishDate}</span></div>
             <div><strong>Published Date : </strong> <span>{updateDate}</span></div>
             <div><strong>Section : </strong> <span>{section}</span></div>
